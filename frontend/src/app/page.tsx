8eff317a4a04c2e80f60cf5c7ff0778b2229f74e
shopnilashik <shopnil.ashik@gmail.com>
2024-09-04 19:37:14 +0600

    layout fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,19 @@
-import { Button } from '@ui5/webcomponents-react';
+import { Button } from "@ui5/webcomponents-react";
 
 export default function Home() {
-  return (
-    <main>
-      <div className="">
-        <p>Hello World</p>
-        <Button>Hello world!</Button>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="m-10">
+                <Button design="Default" className="h-40 w-40">
+                    Customers
+                </Button>
+                <Button design="Default" className="h-40 ml-5 w-40">
+                    Customers
+                </Button>
+                <Button design="Default" className="h-40 ml-5 w-40">
+                    Customers
+                </Button>
+            </div>
+        </main>
+    );
 }
